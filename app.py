69bb5908774a956409880807884782fef78ea9ce
Roman Grubic <grubic.roman@gmail.com>
2020-02-17 11:22:28 +0000

    Added buy and sell page. Connected them app.py file and landing-page.

--- app.py
+++ app.py
@@ -12,6 +12,25 @@ app.config["MONGO_URI"] = os.environ.get('MONGO_DATA_URI')
 
 mongo = PyMongo(app)
 
+
+@app.route('/')
+# --------- Landing page // Home page ---------------------------
+@app.route('/landing-page')
+def landing_page():
+    return render_template('landing-page.html')
+
+
+# --------- Buying page -----------------------------------------
+@app.route('/buy')
+def buy():
+    return render_template('buy.html')
+
+
+# --------- Selling page ----------------------------------------
+@app.route('/sell')
+def sell():
+    return render_template('sell.html')
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
