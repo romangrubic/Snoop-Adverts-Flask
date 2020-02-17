167429e2b0bbda8fd025c59d2f72ce8726893bbb
Roman Grubic <grubic.roman@gmail.com>
2020-02-17 10:40:14 +0000

    Added base.html, css and js file and appropriate links to Bootstrap and jQuery.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 from os import path
 if path.exists("env.py"):
   import env 
