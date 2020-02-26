075240f780d2a259d0f179ea601281e8118d5910
Roman Grubic <grubic.roman@gmail.com>
2020-02-26 10:58:48 +0000

    Python code successfully passing PEP8.

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
+import math
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo, DESCENDING
 from bson.objectid import ObjectId
 from os import path
 if path.exists("env.py"):
     import env
-import math
+
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'adverts'
@@ -15,20 +16,23 @@ mongo = PyMongo(app)
 
 ADS_PER_PAGE = 12
 
+
 @app.route('/')
 # --------- Home page -------------------------------------------
 @app.route('/home')
 def home():
     total_view()
-    return render_template('home.html', 
-                            tittle="Home", 
-                            views=mongo.db.total_view.find(),
-                            top_ads=mongo.db.advert.find().sort('views', DESCENDING).limit(4))
+    return render_template('home.html',
+                           tittle="Home",
+                           views=mongo.db.total_view.find(),
+                           top_ads=mongo.db.advert.find()
+                           .sort('views', DESCENDING).limit(4))
 
 
 def total_view():
     total_view = mongo.db.total_view
-    total_view.update({'_id': ObjectId('5e4edcbd1c9d440000e3b783')}, {'$inc': {'views': 1}})
+    total_view.update({'_id': ObjectId('5e4edcbd1c9d440000e3b783')}, {
+                      '$inc': {'views': 1}})
     return True
 
 
@@ -40,8 +44,15 @@ def marketplace():
     ads_count = mongo.db.advert.find().count()
     page_count = int(math.ceil(ads_count / ADS_PER_PAGE))
     page_numbers = range(1, page_count + 1)
-    ads_on_page = mongo.db.advert.find().sort('views', DESCENDING).skip(ads_to_skip).limit(ADS_PER_PAGE)
-    return render_template('marketplace.html', tittle="Marketplace", adverts=ads_on_page, ads=ads_on_page, page=page_number, pages=page_numbers, total=page_count)
+    ads_on_page = mongo.db.advert.find().sort(
+        'views', DESCENDING).skip(ads_to_skip).limit(ADS_PER_PAGE)
+    return render_template('marketplace.html',
+                           tittle="Marketplace",
+                           adverts=ads_on_page,
+                           ads=ads_on_page,
+                           page=page_number,
+                           pages=page_numbers,
+                           total=page_count)
 
 
 # --------- Filter: Motors ------------------------------------
@@ -49,11 +60,20 @@ def marketplace():
 def motors_and_vehicles():
     page_number = int(request.args.get('page', 1))
     ads_to_skip = (page_number - 1) * ADS_PER_PAGE
-    ads_count = mongo.db.advert.find({'category_name': 'Motors and vehicles'}).count()
+    ads_count = mongo.db.advert.find(
+        {'category_name': 'Motors and vehicles'}).count()
     page_count = int(math.ceil(ads_count / ADS_PER_PAGE))
     page_numbers = range(1, page_count + 1)
-    ads_on_page = mongo.db.advert.find({'category_name': 'Motors and vehicles'}).sort('views', DESCENDING).skip(ads_to_skip).limit(ADS_PER_PAGE)
-    return render_template('marketplace.html', tittle="Motors and vehicles", adverts=ads_on_page, ads=ads_on_page, page=page_number, pages=page_numbers, total=page_count)
+    ads_on_page = mongo.db.advert.find(
+        {'category_name': 'Motors and vehicles'}).sort(
+        'views', DESCENDING).skip(ads_to_skip).limit(ADS_PER_PAGE)
+    return render_template('marketplace.html',
+                           tittle="Motors and vehicles",
+                           adverts=ads_on_page,
+                           ads=ads_on_page,
+                           page=page_number,
+                           pages=page_numbers,
+                           total=page_count)
 
 
 # --------- Filter: Home ------------------------------------
@@ -61,11 +81,20 @@ def motors_and_vehicles():
 def home_garden_diy():
     page_number = int(request.args.get('page', 1))
     ads_to_skip = (page_number - 1) * ADS_PER_PAGE
-    ads_count = mongo.db.advert.find({'category_name': 'Home, garden, DIY'}).count()
+    ads_count = mongo.db.advert.find(
+        {'category_name': 'Home, garden, DIY'}).count()
     page_count = int(math.ceil(ads_count / ADS_PER_PAGE))
     page_numbers = range(1, page_count + 1)
-    ads_on_page = mongo.db.advert.find({'category_name': 'Home, garden, DIY'}).sort('views', DESCENDING).skip(ads_to_skip).limit(ADS_PER_PAGE)
-    return render_template('marketplace.html', tittle="Home, garden and DIY", adverts=ads_on_page, ads=ads_on_page, page=page_number, pages=page_numbers, total=page_count)
+    ads_on_page = mongo.db.advert.find(
+        {'category_name': 'Home, garden, DIY'}).sort('views', DESCENDING).skip(
+            ads_to_skip).limit(ADS_PER_PAGE)
+    return render_template('marketplace.html',
+                           tittle="Home, garden and DIY",
+                           adverts=ads_on_page,
+                           ads=ads_on_page,
+                           page=page_number,
+                           pages=page_numbers,
+                           total=page_count)
 
 
 # --------- Filter: PC stuff-----------------------------------
@@ -73,11 +102,19 @@ def home_garden_diy():
 def electronics():
     page_number = int(request.args.get('page', 1))
     ads_to_skip = (page_number - 1) * ADS_PER_PAGE
-    ads_count = mongo.db.advert.find({'category_name': 'Electronics, mobile, PC'}).count()
+    ads_count = mongo.db.advert.find(
+        {'category_name': 'Electronics, mobile, PC'}).count()
     page_count = int(math.ceil(ads_count / ADS_PER_PAGE))
     page_numbers = range(1, page_count + 1)
-    ads_on_page = mongo.db.advert.find({'category_name': 'Electronics, mobile, PC'}).sort('views', DESCENDING).skip(ads_to_skip).limit(ADS_PER_PAGE)
-    return render_template('marketplace.html', tittle="Electronics, mobile and PC", adverts=ads_on_page, ads=ads_on_page, page=page_number, pages=page_numbers, total=page_count)
+    ads_on_page = mongo.db.advert.find(
+        {'category_name': 'Electronics, mobile, PC'}
+    ).sort('views', DESCENDING).skip(ads_to_skip).limit(ADS_PER_PAGE)
+    return render_template('marketplace.html',
+                           tittle="Electronics, mobile and PC",
+                           adverts=ads_on_page, ads=ads_on_page,
+                           page=page_number,
+                           pages=page_numbers,
+                           total=page_count)
 
 
 # ----------- Filter: Search ------------------------------------
@@ -89,11 +126,21 @@ def search():
 
     page_number = int(request.args.get('page', 1))
     ads_to_skip = (page_number - 1) * ADS_PER_PAGE
-    ads_count = mongo.db.advert.find({'advert_name': {"$regex": query}}).count()
+    ads_count = mongo.db.advert.find(
+        {'advert_name': {"$regex": query}}).count()
     page_count = int(math.ceil(ads_count / ADS_PER_PAGE))
     page_numbers = range(1, page_count + 1)
-    ads_on_page = mongo.db.advert.find({'advert_name': {"$regex": query}}).skip(ads_to_skip).limit(ADS_PER_PAGE)
-    return render_template('search.html', tittle="Search", results=ads_on_page, ads=ads_on_page, results_number=results_number, page=page_number, pages=page_numbers, total=page_count)
+    ads_on_page = mongo.db.advert.find(
+        {'advert_name': {"$regex": query}}).skip(
+        ads_to_skip).limit(ADS_PER_PAGE)
+    return render_template('search.html',
+                           tittle="Search",
+                           results=ads_on_page,
+                           ads=ads_on_page,
+                           results_number=results_number,
+                           page=page_number,
+                           pages=page_numbers,
+                           total=page_count)
 
 
 # --------- Single advert page ----------------------------------
@@ -101,7 +148,9 @@ def search():
 def view_advert(advert_id):
     advert = mongo.db.advert.find_one({'_id': ObjectId(advert_id)})
     view_count(advert_id)
-    return render_template('view_advert.html', tittle="Advert info", advert=advert)
+    return render_template('view_advert.html',
+                           tittle="Advert info",
+                           advert=advert)
 
 
 def view_count(advert_id):
@@ -113,7 +162,10 @@ def view_count(advert_id):
 # ----------- Add advert ----------------------------------------
 @app.route('/add_advert')
 def add_advert():
-    return render_template('add_advert.html', tittle="Add advert", categories=mongo.db.categories.find(), counties=mongo.db.county.find())
+    return render_template('add_advert.html',
+                           tittle="Add advert",
+                           categories=mongo.db.categories.find(),
+                           counties=mongo.db.county.find())
 
 
 # ---------- Insert advert --------------------------------------
@@ -150,7 +202,11 @@ def file(filename):
 def edit_advert(advert_id):
     the_advert = mongo.db.advert.find_one({"_id": ObjectId(advert_id)})
     all_categories = mongo.db.categories.find()
-    return render_template('edit_advert.html', tittle="Edit Advert",  advert=the_advert, categories=all_categories, counties=mongo.db.county.find())
+    return render_template('edit_advert.html',
+                           tittle="Edit Advert",
+                           advert=the_advert,
+                           categories=all_categories,
+                           counties=mongo.db.county.find())
 
 
 # ------------ Update advert -----------------------------------
@@ -158,7 +214,8 @@ def edit_advert(advert_id):
 def update_advert(advert_id):
     advert = mongo.db.advert.find_one({"_id": ObjectId(advert_id)})
 
-    if 'advert_image' in request.files and request.files['advert_image'].filename != "":
+    if 'advert_image' in request.files and request.files[
+            'advert_image'].filename != "":
         advert_image = request.files['advert_image']
         image_filename = advert_image.filename
         mongo.save_file(advert_image.filename, advert_image)
@@ -166,7 +223,7 @@ def update_advert(advert_id):
         image_filename = advert['imageURL']
 
     mongo.db.advert.update({'_id': ObjectId(advert_id)},
-    {
+                           {
         'category_name': request.form.get('category_name'),
         'advert_name': request.form.get('advert_name').upper(),
         'advert_description': request.form.get('advert_description'),
@@ -190,5 +247,5 @@ def delete_advert(advert_id):
 # ------------- Host/Port/Debug --------------------------------
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
-    debug=True)
+            port=int(os.environ.get('PORT')),
+            debug=True)
