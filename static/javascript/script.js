254cf04d29efccc055ccb1aa170e4c1f315b1889
Roman Grubic <grubic.roman@gmail.com>
2020-03-06 13:42:59 +0000

    Added option for user to copy adverts link to clipboard with a button.

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -4,3 +4,13 @@ $(".search-filter").hide();
 $("#more-filters").click(function(){
     $(".search-filter").slideToggle();
 })
+
+/* ---------- Copy link to clipboard ------------ */
+/* ---------- Credits to W3 schools https://www.w3schools.com/howto/howto_js_copy_clipboard.asp -----------*/
+/*---- Changed for my project ----------*/
+function copyURL() {
+  var copyText = document.getElementById("advert-url");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+}
